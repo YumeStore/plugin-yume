c336b638e94d14145f872f003171d23949a0aff1
muuh.renai@gmail.com <muuh.renai@gmail.com>
2021-05-15 23:07:35 -0300

    Alterações cabeçalho autores

--- accessCoursesPlugin/access-courses-plugin.php
+++ accessCoursesPlugin/access-courses-plugin.php
@@ -6,10 +6,11 @@
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Paulo Fernando, Pedro Vski, Murillo Vski
+ * Author:            Murillo Vincente Araujo, Paulo Fernando Lima Ornelas do Amaral, Pedro Baptista dos Santos
  * License:           GPL v2 or later
  * Text Domain: acesso_api_cursos
  */
+include_once 'config/config.php';
 
 if (!function_exists('add_action')) {
     echo _('O plugin não pode ser passado direto');
@@ -20,43 +21,43 @@ require_once(dirname(__FILE__) . '/model/cursos.viewmodel.php');
 function shortCodeListaCursos()
 {
     $cursos = new Cursos();
-    $url_host = filter_input(INPUT_SERVER, 'HTTP_HOST');
-    define('pg', 'http://'.$url_host . '/eduAcademy'); // wp-content\plugins\accessCoursesPlugin/assets/css/bootstrap.min.css
 ?>
-    <link rel="stylesheet" href="<?php  echo pg . '/wp-content/plugins/accessCoursesPlugin/assets/css/bootstrap.min.css' ?>"/>
-    <table class="table table-dark">
-        <thead>
-            <th>ID Curso</th>
-            <th>Nome Curso</th>
-            <th></th>
-        </thead>
-        <tbody>
-            <?php
-            $curso_enviado = filter_input(INPUT_POST, 'AdicionarCurso', FILTER_SANITIZE_STRING);
-            $dados_curso = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-            if($dados_curso){
-               $cursos->insertCoursePost($dados_curso['courseTitle'], $dados_curso['courseDescription']);
-            }
-            
-            foreach ($cursos->getCursos() as $key) {
-            ?>
-                <tr>
-                    <th><?php echo $key->course_id ?></th>
-                    <th><?php echo $key->course_title ?></th>
-                    <th>
-                        <form name="addCourse" method="POST">
-                            <input type="hidden" name="courseTitle" value="<?php echo $key->course_title; ?>" />
-                            <input type="hidden" name="courseDescription" value="<?php echo $key->course_description; ?>" />
-                            <input type="submit" name="addCourse" value="Adicionar Curso" />
-                        </form>
-                    </th>
-                </tr>
-            <?php
-            }
-            ?>
+    <link rel="stylesheet" href="<?php echo pg . '/wp-content/plugins/accessCoursesPlugin/assets/css/bootstrap.min.css' ?>" />
+    <div class="content">
+        <table class="table table-striped table-dark">
+            <thead>
+                <th scope="col">ID Curso</th>
+                <th scope="col">Nome Curso</th>
+                <th scope="col"></th>
+            </thead>
+            <tbody>
+                <?php
+                $curso_enviado = filter_input(INPUT_POST, 'AdicionarCurso', FILTER_SANITIZE_STRING);
+                $dados_curso = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+                if ($dados_curso) {
+                    $cursos->insertCoursePost($dados_curso['courseTitle'], $dados_curso['courseDescription']);
+                }
 
-        </tbody>
-    </table>
+                foreach ($cursos->getCursos() as $key) {
+                ?>
+                    <tr>
+                        <th scope="row"><?php echo $key->course_id ?></th>
+                        <th scope="row"><?php echo $key->course_title ?></th>
+                        <th scope="row">
+                            <form name="addCourse" method="POST">
+                                <input type="hidden" name="courseTitle" value="<?php echo $key->course_title; ?>" />
+                                <input type="hidden" name="courseDescription" value="<?php echo $key->course_description; ?>" />
+                                <input type="submit" class="btn btn-success h-50" name="addCourse" value="Adicionar Curso" />
+                            </form>
+                        </th>
+                    </tr>
+                <?php
+                }
+                ?>
+
+            </tbody>
+        </table>
+    </div>
 <?php
 }
 
