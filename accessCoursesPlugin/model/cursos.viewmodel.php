b91636eca9ab544756086a40c4fc35ec2aad7c01
muuh.renai@gmail.com <muuh.renai@gmail.com>
2021-05-16 22:27:03 -0300

    Alterações consumo API

--- accessCoursesPlugin/model/cursos.viewmodel.php
+++ accessCoursesPlugin/model/cursos.viewmodel.php
@@ -1,13 +1,19 @@
 <?php
 class Cursos
 {
+    var $consumoApi;
+
+    function __construct()
+    {
+        $this->consumoApi = new ConsumoApi('https://www.iped.com.br/', 'b6d5ee6c0bee8cb0e35a33e9677b45afc60d7eff');
+    }
+
     public function getCursos()
     {
         require_once(dirname(__FILE__) . '/../consumo-api/consumo-api.php');
 
-        $consumoApi = new ConsumoApi('https://www.iped.com.br/', 'b6d5ee6c0bee8cb0e35a33e9677b45afc60d7eff');
-
-        $retorno = $consumoApi->retornoListaCursos();
+        
+        $retorno = $this->consumoApi->retornoListaCursos();
         $retorno_array = json_decode($retorno);
 
         return $retorno_array->COURSES;
@@ -25,8 +31,7 @@ class Cursos
             'post_name' => 'novo-curso-wp',
             'post_type' => 'lp_course'
         );
-
-        // Insert the post into the database
+        
         wp_insert_post($my_post);
     }
 }
